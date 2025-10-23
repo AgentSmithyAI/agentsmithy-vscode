9c9be31420197dd2c63870478de5b3c2cab16ba8
archimed <archimed.shaman@gmail.com>
2025-10-23 09:54:47 +0200

    Fix UI

--- src/webview/src/DialogsUI.ts
+++ src/webview/src/DialogsUI.ts
@@ -136,18 +136,18 @@ export class DialogsUI {
           <div class="dialog-item-content">
             <div class="dialog-item-title">${title}</div>
             <div class="dialog-item-meta">${updatedAt}</div>
-          </div>
-          <div class="dialog-item-actions">
-            <button class="dialog-action-btn rename-btn" data-dialog-id="${dialog.id}" title="Rename">
-              <svg viewBox="0 0 16 16" aria-hidden="true">
-                <path d="M13.23 1h-1.46L3.52 9.25l-.16.22L2 13.59l4.12-1.36.22-.16L14.59 3.82V2.36L13.23 1zM3.84 11.72l.71-2.36 1.65 1.65-2.36.71z"/>
-              </svg>
-            </button>
-            <button class="dialog-action-btn delete-btn" data-dialog-id="${dialog.id}" title="Delete">
-              <svg viewBox="0 0 16 16" aria-hidden="true">
-                <path d="M10 3h3v1h-1v9l-1 1H4l-1-1V4H2V3h3V2a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v1zM9 2H6v1h3V2zM4 13h7V4H4v9zm2-8H5v7h1V5zm1 0h1v7H7V5zm2 0h1v7H9V5z"/>
-              </svg>
-            </button>
+            <div class="dialog-item-actions">
+              <button class="dialog-action-btn rename-btn" data-dialog-id="${dialog.id}" title="Rename">
+                <svg viewBox="0 0 16 16" aria-hidden="true">
+                  <path d="M13.23 1h-1.46L3.52 9.25l-.16.22L2 13.59l4.12-1.36.22-.16L14.59 3.82V2.36L13.23 1zM3.84 11.72l.71-2.36 1.65 1.65-2.36.71z"/>
+                </svg>
+              </button>
+              <button class="dialog-action-btn delete-btn" data-dialog-id="${dialog.id}" title="Delete">
+                <svg viewBox="0 0 16 16" aria-hidden="true">
+                  <path d="M10 3h3v1h-1v9l-1 1H4l-1-1V4H2V3h3V2a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v1zM9 2H6v1h3V2zM4 13h7V4H4v9zm2-8H5v7h1V5zm1 0h1v7H7V5zm2 0h1v7H9V5z"/>
+                </svg>
+              </button>
+            </div>
           </div>
         </div>
       `;
